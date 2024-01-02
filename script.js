4387f62f79e5e1f0bd74d130843e82ca46facec5
Flynn Costello <flynn.p.costello@outlook.com>
2024-01-02 14:41:46 +1100

    movie website

---
+++ script.js
@@ -0,0 +1,74 @@
+const tmdbKey = '7e5bd18bb7eb6ae44fdc53405271c0fb';
+const tmdbBaseUrl = 'https://api.themoviedb.org/3';
+const playBtn = document.getElementById('playBtn');
+
+// Gets all genres
+const getGenres = async () => {
+  const genreRequestEndpoint = '/genre/movie/list'
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = tmdbBaseUrl + genreRequestEndpoint + requestParams;
+  try {
+    const response = await fetch(urlToFetch);
+    if(response.ok) {
+      const jsonResponse = await response.json();
+      const genres = jsonResponse.genres;
+      return genres;
+    }
+  } catch(err) {
+    console.log(err);
+  }
+};
+
+// Gets all movies within a genre
+const getMovies = async () => {
+  const selectedGenre = getSelectedGenre();
+  const discoverMovieEndpoint = '/discover/movie';
+  const requestParams = `?api_key=${tmdbKey}&with_genres=${selectedGenre}`;
+  const urlToFetch = tmdbBaseUrl + discoverMovieEndpoint + requestParams;
+  try {
+    const response = await fetch(urlToFetch);
+    if(response.ok) {
+      const jsonResponse = await response.json();
+      const movies = jsonResponse.results;
+      return movies;
+    }
+  } catch(err) {
+    console.log(err);
+  }
+};
+
+// Gets the info of a specific movie
+const getMovieInfo = async movie => {
+  const movieId = movie.id;
+  const movieEndpoint = `/movie/${movieId}`;
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = tmdbBaseUrl + movieEndpoint + requestParams;
+
+  try {
+    const response = await fetch(urlToFetch);
+    if(response.ok) {
+      const movieInfo = await response.json();
+      return movieInfo;
+    }
+  } catch(err) {
+    console.log(err);
+  }
+};
+
+
+// Gets a list of movies and ultimately displays the info of a random movie from the list
+const showRandomMovie = async () => {
+  const movieInfo = document.getElementById('movieInfo');
+  if (movieInfo.childNodes.length > 0) {
+    clearCurrentMovie(); // Removes all content within current movie to be replaced thereafter
+  };
+  const movies = await getMovies();
+  const randomMovie = getRandomMovie(movies);
+  const info = await getMovieInfo(randomMovie);
+  displayMovie(info); // Finally displaying new movie info to screen
+};
+
+getGenres().then(populateGenreDropdown); // Sending promise result to populateGenreDropdown function to create all genres
+playBtn.onclick = showRandomMovie; // Assigning reference to function instead of the function return result
+// So that when the user clicks the let's play button the show random movie function runs
+
